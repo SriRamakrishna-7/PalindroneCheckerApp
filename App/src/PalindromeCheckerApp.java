804f69d85acfe2529773cdd86fdc7e94b0efc0cc
Sri Ramakrishna <you@example.com>
2026-02-23 15:53:16 +0530

    uc2 palindromr

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,20 @@
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker App Management System");
-        System.out.println("Modify this logic to usecase 2");
+        String word = "madam";
+
+        if (isPalindrome(word)) {
+            System.out.println(word + " is a palindrome");
+        } else {
+            System.out.println(word + " is NOT a palindrome");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        return str.equalsIgnoreCase(reversed);
     }
 }
