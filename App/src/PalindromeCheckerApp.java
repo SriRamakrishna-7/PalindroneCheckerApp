c47d67bf532212fe9bd4078ff8b61fd8f5b4dc2c
Sri Ramakrishna <you@example.com>
2026-03-17 14:41:07 +0530

    Welcome to UC7

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,36 +1,28 @@
-import java.util.Scanner;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class PalindromeCheckerApp {
 
-    public static boolean isPalindrome(String input) {
-        char[] charArray = input.toCharArray();
-        int start = 0;
-        int end = charArray.length - 1;
-
-        while (start < end) {
-            if (charArray[start] != charArray[end]) {
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
-    }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
+        String input = "racecar";
 
-        input = input.replaceAll("\\s+", "").toLowerCase();
+        Deque<Character> deque = new ArrayDeque<>();
 
-        if (isPalindrome(input)) {
-            System.out.println("The given string is a Palindrome.");
-        } else {
-            System.out.println("The given string is NOT a Palindrome.");
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
         }
 
-        scanner.close();
+        boolean isPalindrome = true;
+
+        while (deque.size() > 1) {
+            if (deque.pollFirst() != deque.pollLast()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
